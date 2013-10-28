9b891e7cf220f9f28f6f68587f07f017d7977977
mzehrer <zehrer@zepan.org>
2013-10-28 09:52:58 +0100

    Spring objectify injection

--- src/main/java/org/zepan/hackavote/ApiService.java
+++ src/main/java/org/zepan/hackavote/ApiService.java
@@ -1,9 +1,11 @@
 package org.zepan.hackavote;
 
 import com.googlecode.objectify.Key;
+import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.Ref;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.zepan.hackavote.domain.Ballot;
 import org.zepan.hackavote.domain.BallotEntry;
@@ -15,13 +17,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 
-import static org.zepan.hackavote.objectify.OfyService.ofy;
-
 @Service
 public class ApiService {
 
     protected final Logger log = LoggerFactory.getLogger(ApiService.class);
 
+    @Autowired
+    private com.googlecode.objectify.ObjectifyFactory objectifyFactory;
 
     @GET
     @Path("/elections")
@@ -31,7 +33,9 @@ public class ApiService {
 
         log.debug("getting all elections");
 
-        List<Election> elections = ofy().load().type(Election.class).list();
+        Objectify ofy = objectifyFactory.begin();
+
+        List<Election> elections = ofy.load().type(Election.class).list();
         if (elections == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         } else {
@@ -49,7 +53,10 @@ public class ApiService {
 
 
         log.debug("getting electionid : " + id);
-        Election election = ofy().load().type(Election.class).id(id).now();
+
+        Objectify ofy = objectifyFactory.begin();
+
+        Election election = ofy.load().type(Election.class).id(id).now();
         if (election == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         } else {
@@ -66,12 +73,15 @@ public class ApiService {
 
         log.debug("adding : " + election.toString());
 
+        Objectify ofy = objectifyFactory.begin();
+
         Ballot ballot = new Ballot();
         ballot.setName(election.getName());
-        Key<Ballot> ballotkey = ofy().save().entity(ballot).now();
+
+        Key<Ballot> ballotkey = ofy.save().entity(ballot).now();
         election.setBallot(Ref.create(ballotkey));
 
-        Key<Election> key = ofy().save().entity(election).now();
+        Key<Election> key = ofy.save().entity(election).now();
         if (key == null) {
             return Response.status(Response.Status.BAD_REQUEST).build();
         } else {
@@ -87,13 +97,16 @@ public class ApiService {
 
 
         log.debug("updating electionid : " + id);
-        Election savedelection = ofy().load().type(Election.class).id(id).now();
+
+        Objectify ofy = objectifyFactory.begin();
+
+        Election savedelection = ofy.load().type(Election.class).id(id).now();
         if (savedelection == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         } else {
             savedelection.setName(election.getName());
             savedelection.setClosed(election.isClosed());
-            ofy().save().entity(savedelection).now();
+            ofy.save().entity(savedelection).now();
             return Response.ok(savedelection).build();
         }
     }
@@ -105,7 +118,10 @@ public class ApiService {
     public Response deleteBook(@PathParam("id") Long id) {
 
         log.debug("deleting electionid : " + id);
-        ofy().delete().type(Election.class).id(id).now();
+
+        Objectify ofy = objectifyFactory.begin();
+
+        ofy.delete().type(Election.class).id(id).now();
         return Response.ok().build();
     }
 
@@ -118,7 +134,9 @@ public class ApiService {
 
         log.debug("getting all all ballots for election : " + electionid);
 
-        Election election = ofy().load().type(Election.class).id(electionid).now();
+        Objectify ofy = objectifyFactory.begin();
+
+        Election election = ofy.load().type(Election.class).id(electionid).now();
         if (election == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         } else {
@@ -146,7 +164,9 @@ public class ApiService {
 
         log.debug("adding : " + ballotentry.toString() + " entry for ballot to election : " + electionid);
 
-        Election election = ofy().load().type(Election.class).id(electionid).now();
+        Objectify ofy = objectifyFactory.begin();
+
+        Election election = ofy.load().type(Election.class).id(electionid).now();
         if (election == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         } else {
@@ -155,9 +175,9 @@ public class ApiService {
             if (ballotref == null) {
                 return Response.status(Response.Status.NOT_FOUND).build();
             } else {
-                Key<BallotEntry> ballotentrykey = ofy().save().entity(ballotentry).now();
+                Key<BallotEntry> ballotentrykey = ofy.save().entity(ballotentry).now();
                 ballotref.get().addEntry(Ref.create(ballotentrykey));
-                ofy().save().entity(ballotref.get()).now();
+                ofy.save().entity(ballotref.get()).now();
 
                 return Response.ok(ballotentry).build();
             }
@@ -173,11 +193,13 @@ public class ApiService {
 
         log.debug("getting all all ballots for election : " + electionid);
 
-        Election election = ofy().load().type(Election.class).id(electionid).now();
+        Objectify ofy = objectifyFactory.begin();
+
+        Election election = ofy.load().type(Election.class).id(electionid).now();
         if (election == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         } else {
-            BallotEntry ballotentry = ofy().load().type(BallotEntry.class).id(entryid).now();
+            BallotEntry ballotentry = ofy.load().type(BallotEntry.class).id(entryid).now();
 
             if (ballotentry == null)
                 return Response.status(Response.Status.NOT_FOUND).build();
@@ -195,19 +217,18 @@ public class ApiService {
 
         log.debug("getting all all ballots for election : " + electionid);
 
-        Election election = ofy().load().type(Election.class).id(electionid).now();
+        Objectify ofy = objectifyFactory.begin();
+
+        Election election = ofy.load().type(Election.class).id(electionid).now();
         if (election == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         } else {
-            //BallotEntry ballotentry = ofy().load().type(BallotEntry.class).id(entryid).now();
+            //BallotEntry ballotentry = ofy.load().type(BallotEntry.class).id(entryid).now();
 
-            ofy().delete().type(BallotEntry.class).id(entryid).now();
+            ofy.delete().type(BallotEntry.class).id(entryid).now();
 
             return Response.ok().build();
         }
 
     }
-
-
-
 }
